23ffc4c0c7ced42cad463a2cc35ee17d30c6e8d6
ZiganshinIB <elzig3012@gmail.com>
2024-07-06 10:31:18 +0300

    Refract Exception in vk_message_bot

--- vk_message_bot.py
+++ vk_message_bot.py
@@ -11,11 +11,8 @@ logger = logging.getLogger(__name__)
 
 def dialog_flow_response(event, vk_api) -> None:
     """Echo the user message."""
-    try:
-        response_text = get_dialog_response(event.text, event.user_id)['response_text']
-        vk_api.messages.send(user_id=event.user_id, message=response_text, random_id=0)
-    except Exception as e:
-        logger.error("VK: " + str(e), exc_info=True)
+    response_text = get_dialog_response(event.text, event.user_id)['response_text']
+    vk_api.messages.send(user_id=event.user_id, message=response_text, random_id=0)
 
 
 def main() -> None:
@@ -25,14 +22,14 @@ def main() -> None:
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     logger.addHandler(MyLogsHandler())
-    try:
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                dialog_flow_response(event, vk_api)
-    except Exception as e:
-        logger.error("Бот VK перестала работать: " + str(e), exc_info=True)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            dialog_flow_response(event, vk_api)
  
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.error("Бот VK перестала работать: " + str(e), exc_info=True)
 
